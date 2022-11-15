6f41e02677cf9632efb8455404e4ddbf43cf733a
lucas <lucasmartinezv@gmail.com>
2022-11-15 00:07:48 -0300

    primero

--- services/main.py
+++ services/main.py
@@ -1,13 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def index():
-    return {
-        "Su red está siendo atacada"
-    }
+    return """
+    <html>
+        <body>
+            <h1>Su red está siendo atacada</h1>
+            <p>Estado de la red: Inactiva</p>
+            <p>Tipo de Ataque: DDOS</p>
+            <p>Sistemas comprometidos: 192.168.1.14, 192.168.1.15, 192.168.1.15"</p>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     uvicorn.run(app)
+    # uvicorn.run(app, host="0.0.0.0", port=80)
